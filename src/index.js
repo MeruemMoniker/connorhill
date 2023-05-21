b6a11eae2e857dcde7449c2e9a13148abbaf0002
James Hill <jamesconnorhill@gmail.com>
2023-05-20 20:04:30 -0400

    new commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { HashRouter } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,11 +11,5 @@ root.render(
   </React.StrictMode>
 );
 
-const root2 = ReactDOM.createRoot(document.getElementById('root2'));
-root2.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
 
